6aab135f026d901f7d505f08404c4ee73b109cc6
Yonic Surny <yonic.surny@bnpparibasfortis.com>
2021-05-27 17:18:36 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Yonic Surny on 27/05/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
