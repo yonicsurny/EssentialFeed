7dccf41b99bc9bca1755f6a9f99f457b4b6e1ed0
Yonic Surny <ysu@aelptos.com>
2021-07-04 10:51:14 +0200

    Replace optional params with result enum

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
     func get(from url: URL,
-             completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+             completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -27,12 +32,13 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-                return
+            case .failure:
+                completion(.connectivity)
             }
-            completion(.connectivity)
         }
     }
 }
