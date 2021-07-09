915d93d0ff9e2e6855982413dec36dfd39d4ebb2
Yonic Surny <ysu@aelptos.com>
2021-07-09 16:26:08 +0200

    Add end-to-end API tests in a separate test target

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,91 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Yonic Surny on 09/07/2021.
+//
+
+import XCTest
+import EssentialFeed
+
+/// Run the local server using `npm run start` from the _LocalServer_ folder
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "http://localhost:3000/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        let expectation = expectation(description: "Wait for completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+        
+        switch receivedResult! {
+        case let .success(items):
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+            
+            XCTAssertEqual(items[0], expectedItem(at: 0))
+            XCTAssertEqual(items[1], expectedItem(at: 1))
+            XCTAssertEqual(items[2], expectedItem(at: 2))
+            XCTAssertEqual(items[3], expectedItem(at: 3))
+            XCTAssertEqual(items[4], expectedItem(at: 4))
+            XCTAssertEqual(items[5], expectedItem(at: 5))
+            XCTAssertEqual(items[6], expectedItem(at: 6))
+            XCTAssertEqual(items[7], expectedItem(at: 7))
+            
+        case let .failure(error):
+            XCTFail("Expected successful feed result, got \(error) instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func expectedItem(at index: Int) -> FeedItem {
+        return FeedItem(id: id(at: index),
+                        description: description(at: index),
+                        location: location(at: index),
+                        imageURL: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        if index < 0 || index > 7 { return UUID() }
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        switch index {
+        case 0, 2, 4, 5, 6, 7:
+            return "Description \(index + 1)"
+        default:
+            return nil
+        }
+    }
+    
+    private func location(at index: Int) -> String? {
+        switch index {
+        case 0, 1, 4, 5, 6, 7:
+            return "Location \(index + 1)"
+        default:
+            return nil
+        }
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index + 1).com")!
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # EssentialFeed
 
 This is a learning project to practice concepts presented in the [iOS Lead Essential program](https://www.essentialdeveloper.com)
+
+## End to end testing
+
+Start the local server from the terminal in the _LocalServer_ folder:
+
+```sh
+npm run start
+```
