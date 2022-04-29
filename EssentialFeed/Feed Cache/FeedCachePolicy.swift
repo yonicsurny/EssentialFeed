b18f0b4a585e1d8f3e3ff72c31c0cdbff063fb92
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-04-29 16:58:53 +0200

    Move `FeedCachePolicy` to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Yonic Surny on 29/04/2022.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
