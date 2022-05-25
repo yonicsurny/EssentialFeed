e0fcea712a5cb001bd1fa29aac5d4a5b9b28ec8f
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-05-25 19:34:58 +0200

    `FeedPresenter` displays no error on `didStartLoadingFeed`

--- EssentialFeediOSTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeediOSTests/Feed Presentation/FeedPresenterTests.swift
@@ -4,8 +4,28 @@
 
 import XCTest
 
+struct FeedErrorViewModel {
+    let message: String?
+
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
-    init(view: Any) {}
+    private let errorView: FeedErrorView
+
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
+    }
 }
 
 final class FeedPresenterTests: XCTestCase {
@@ -14,18 +34,34 @@ final class FeedPresenterTests: XCTestCase {
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
+
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+
+        sut.didStartLoadingFeed()
+
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
 }
 
 private extension FeedPresenterTests {
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
 
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: FeedErrorView {
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+
+        private(set) var messages = [Message]()
+
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
