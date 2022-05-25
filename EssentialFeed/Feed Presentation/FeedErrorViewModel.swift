cbf5205f317114b4c9866e5921c01e943ef618a7
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-05-25 21:53:39 +0200

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © Aelptos. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedErrorViewModel {
+    public let message: String?
+
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
