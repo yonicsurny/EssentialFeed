7aaa56928c74b9aef022cc6a7423925748e80523
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-06-24 12:53:30 +0200

    Compose UI

--- LocalServer/server.js
+++ LocalServer/server.js
@@ -8,14 +8,24 @@ function log(message) {
   console.log(date + " // " + message)
 }
 
-app.get('/test-api/feed', (req, res) => {
+app.get('/api/v1/feed', (req, res) => {
   log("Sending response for " + req.url)
   res.sendFile(path.join(__dirname + '/Responses/feed.json'))
 })
+app.get('/api/v1/image/:id', (req, res) => {
+  log("Sending response for " + req.url)
+  res.sendFile(path.join(__dirname + '/Responses/images/' + req.params.id + '.jpg'))
+})
+
+app.get('/test-api/feed', (req, res) => {
+  log("Sending response for " + req.url)
+  res.sendFile(path.join(__dirname + '/Samples/feed.json'))
+})
 app.get('/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image', (req, res) => {
   log("Sending response for " + req.url)
-  res.sendFile(path.join(__dirname + '/Responses/image.png'))
+  res.sendFile(path.join(__dirname + '/Samples/image.png'))
 })
+
 app.all('*', (req, res) => {
   log("Resource not found for " + req.url)
   res.status(404).send('Not found')
