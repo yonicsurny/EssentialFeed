2f20f5ae0b3a6bed95e668ddd086f7b8fdd07d61
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-06-24 10:55:46 +0200

    Add test to guarantee that `CoreDataFeedStore` side effects run serially to prevent unexpected behavior

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -43,6 +43,24 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
 
         expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: url)
     }
+
+    func test_sideEffects_runSerially() {
+        let sut = makeSUT()
+        let url = anyURL()
+
+        let op1 = expectation(description: "Operation 1")
+        sut.insert([localImage(url: url)], timestamp: Date()) { _ in
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "Operation 2")
+        sut.insert(anyData(), for: url) { _ in op2.fulfill() }
+
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(anyData(), for: url) { _ in op3.fulfill() }
+
+        wait(for: [op1, op2, op3], timeout: 5.0, enforceOrder: true)
+    }
 }
 
 private extension CoreDataFeedImageDataStoreTests {
@@ -72,11 +90,12 @@ private extension CoreDataFeedImageDataStoreTests {
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                exp.fulfill()
 
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+                exp.fulfill()
             }
-            exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
