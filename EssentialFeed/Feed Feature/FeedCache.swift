61fa7ec8d28e62e1d3e79440a06ba53c238f27e1
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-06-26 14:34:30 +0200

    Move `FeedCache` to the Feed Feature production group

---
+++ EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © Aelptos. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedCache {
+    typealias Result = Swift.Result<Void, Error>
+
+    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
