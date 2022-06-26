676e57323a7f09da4eaa92c29236d498c3525555
Yonic Surny <562587+aelptos@users.noreply.github.com>
2022-06-26 11:02:51 +0200

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` delivers error on both primary and fallback loader failure

--- EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -107,6 +107,15 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
             fallbackLoader.complete(with: fallbackData)
         })
     }
+
+    func test_loadImageData_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+
+        expect(sut, toCompleteWith: .failure(anyNSError()), when: {
+            primaryLoader.complete(with: anyNSError())
+            fallbackLoader.complete(with: anyNSError())
+        })
+    }
 }
 
 private extension FeedImageDataLoaderWithFallbackCompositeTests {
